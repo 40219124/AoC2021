82150fd8561914f4e75e6b4cdc30162743154eb1
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-12-03 16:32:53 +0000

    D3P2

--- AdventOfCode2021/AdventOfCode2021/AdventOfCode2021.cpp
+++ AdventOfCode2021/AdventOfCode2021/AdventOfCode2021.cpp
@@ -155,6 +155,89 @@ void D3P1() {
 
 
 function<void(void)> Problems[25][2] = { {D1P1, D1P2 }, {D2P1, D2P2}, {D3P1,} };
+void D3P2() {
+	ifstream file(FileFolder + "D3P1.txt");
+	if (file.is_open()) {
+		string line;
+		const int bitCount = 12;
+		vector<string> _oldO2List = {}, _newO2List = {}, _oldCo2List = {}, _newCo2List = {};
+		vector<string>* oldO2List = &_oldO2List, * newO2List = &_newO2List,
+			* oldCo2List = &_oldCo2List, * newCo2List = &_newCo2List,
+			* tempList = nullptr;
+		int oneDoubleCount = 0, lineCount = 0, originalODC, originalLC;
+		while (getline(file, line)) {
+			_oldO2List.push_back(line);
+			_oldCo2List.push_back(line);
+			oneDoubleCount += (line[0] == '1') * 2;
+			lineCount++;
+		}
+
+		originalODC = oneDoubleCount;
+		originalLC = lineCount;
+
+		for (int b = 0; b < bitCount && oldO2List->size() > 1; ++b) {
+			newO2List->clear();
+
+			char common = '1';
+			if (oneDoubleCount < lineCount) {
+				common = '0';
+			}
+			oneDoubleCount = lineCount = 0;
+
+			for (int i = 0; i < oldO2List->size(); ++i) {
+				if ((*oldO2List)[i][b] == common) {
+					newO2List->push_back((*oldO2List)[i]);
+					if (b + 1 < bitCount) {
+						oneDoubleCount += ((*oldO2List)[i][b + 1] == '1') * 2;
+						lineCount++;
+					}
+				}
+			}
+			tempList = oldO2List;
+			oldO2List = newO2List;
+			newO2List = tempList;
+		}
+
+		oneDoubleCount = originalODC;
+		lineCount = originalLC;
+
+		for (int b = 0; b < bitCount && oldCo2List->size() > 1; ++b) {
+			newCo2List->clear();
+
+			char common = '0';
+			if (oneDoubleCount < lineCount) {
+				common = '1';
+			}
+			oneDoubleCount = lineCount = 0;
+
+			for (int i = 0; i < oldCo2List->size(); ++i) {
+				if ((*oldCo2List)[i][b] == common) {
+					newCo2List->push_back((*oldCo2List)[i]);
+					if (b + 1 < bitCount) {
+						oneDoubleCount += ((*oldCo2List)[i][b + 1] == '1') * 2;
+						lineCount++;
+					}
+				}
+			}
+			tempList = oldCo2List;
+			oldCo2List = newCo2List;
+			newCo2List = tempList;
+		}
+
+		int o2 = 0, co2 = 0, bVal = 1;
+		for (int i = bitCount - 1; i >= 0; --i) {
+			o2 += ((*oldO2List)[0][i] - '0') * bVal;
+			co2 += ((*oldCo2List)[0][i] - '0') * bVal;
+			bVal *= 2;
+		}
+
+		cout << "o2: " << o2 << ", co2: " << co2 << endl;
+		cout << "answer: " << o2 * co2 << endl;
+	}
+}
+
+
+function<void(void)> Problems[25][2] = { {D1P1, D1P2 }, {D2P1, D2P2}, {D3P1, D3P2} };
 
 int main(int argc, char** argv)
 {
